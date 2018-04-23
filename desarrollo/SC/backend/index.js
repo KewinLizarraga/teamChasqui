935130844860ebb2754cf4b73c372a8a74c45384
Joshua Navarro <joshua.navarro35@gmail.com>
2018-04-23 12:14:03 -0500

    add some packages

--- desarrollo/SC/backend/index.js
+++ desarrollo/SC/backend/index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
 
 const { config } = require('./config/keys');
+
+const app = express();
 
 mongoose.connect(config.mongoURI)
   .then(db => console.log('Connected to MongoDB'))
   .catch(err => console.log(err));
 
-const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(logger('dev'));
 
 app.get('/', (req, res) => {
   res.send('Chasqui backend');
