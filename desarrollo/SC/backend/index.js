169ef23d8ce51ef959e1a4333c80930c36e02b8a
Joshua Navarro <joshua.navarro35@gmail.com>
2018-05-13 15:58:52 -0500

    allow acces for all origins

--- desarrollo/SC/backend/index.js
+++ desarrollo/SC/backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const { config } = require('./config/keys');
 
@@ -13,6 +14,7 @@ mongoose.connect(config.mongoURI)
   .then(db => console.log('Connected to MongoDB'))
   .catch(err => { throw err });
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
