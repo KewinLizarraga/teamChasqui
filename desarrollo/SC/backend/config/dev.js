ec75c5835bcf6436e03eba4aedbd54e536c2ff3b
Joshua Navarro <joshua.navarro35@gmail.com>
2018-06-23 13:00:08 -0500

    commit

---
+++ desarrollo/SC/backend/config/dev.js
@@ -0,0 +1,18 @@
+module.exports = {
+  config: {
+    mongoURI: 'mongodb://joshua:password@ds247699.mlab.com:47699/chasqui-dev-db'
+  },
+  keys: {
+    jwtKeySecret: 'letmein123',
+    sendGridUser: 'joshnav35',
+    sendGridPass: 'carlospunk1',
+    facebook: {
+      clientID: '1075549959251532',
+      clientSecret: 'a00a561cf42f1a017056b309f0f553a7',
+    },
+    google: {
+      clientID: '1089183054482-56j4oimv0km8jdu8dr74cuh1usuo6kc9.apps.googleusercontent.com',
+      clientSecret: 'x0FKWRpbV6ojj_96HKsRo74K',
+    }
+  }
+}
